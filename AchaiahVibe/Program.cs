c48e796e04308fd5958661ad351687bf4c3781de
Rafael Difini <rafaeldifini@live.com>
2022-04-08 20:09:21 -0300

    Add files via upload
    Versao do app aplicando POO

---
+++ AchaiahVibe/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+namespace AchaiahVibe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Produto produto = new Produto();
+
+            produto.CalculaProduto();
+            produto.AplicaPorcentagem();
+            produto.CalculaProduto();
+        }
+    }
+}
